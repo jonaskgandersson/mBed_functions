02163c38e9cddee1c45e4a231d891f946503a69b
Jonas Andersson <jonaskgandersson@gmail.com>
2017-11-27 22:14:56 +0100

    Update DST.h

--- DST.h
+++ DST.h
@@ -2,19 +2,37 @@
 /* Returns true if date is in daylight saving time
    DST last sunday in March to last sunday in October
    
+   hour: current hour(0-23)
    day: current day in mounth
    month: current month
    dow: current day of week( 0=Sunday )
 */
-bool IsDst(int day, int month, int dow)
+bool IsDst(int hour, int day, int month, int dow)
 {
      if (month < 3 || month > 10)  return false; 
      if (month > 3 && month < 10)  return true; 
 
      int previousSunday = day - dow;
 
-     if (month == 3) return previousSunday >= 25;
-     if (month == 10) return previousSunday < 25;
+     if (month == 3)
+     {
+        if( (previousSunday >= 25) && (dow == 0) && (hour>=2) )
+        {
+           return FALSE;
+        }
+        else if(previousSunday >= 25)
+        {
+           return FALSE;
+        }
+        else
+        {
+           return TRUE;
+        }
+     }
+           
+     if (month == 10)
+     {
+        if( (previousSunday < 25) && (dow // To do
 
      return false; // this line never gonna happend
  }
