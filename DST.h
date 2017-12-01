d95508997a5a08d79baa7d33e0066e2695cb8f83
Jonas Andersson <jonaskgandersson@gmail.com>
2017-12-01 09:46:16 +0100

    Update DST.h

--- DST.h
+++ DST.h
@@ -7,32 +7,15 @@
    month: current month
    dow: current day of week( 0=Sunday )
 */
-bool IsDst(int hour, int day, int month, int dow)
-{
+bool IsDst(int day, int month, int dow)
+ {
      if (month < 3 || month > 10)  return false; 
      if (month > 3 && month < 10)  return true; 
 
      int previousSunday = day - dow;
 
-     if (month == 3)
-     {
-        if( (previousSunday >= 25) && (dow == 0) && (hour>=2) )
-        {
-           return FALSE;
-        }
-        else if(previousSunday >= 25)
-        {
-           return FALSE;
-        }
-        else
-        {
-           return TRUE;
-        }
-     }
-           
-     if (month == 10)
-     {
-        if( (previousSunday < 25) && (dow // To do
+     if (month == 3) return previousSunday >= 25;
+     if (month == 10) return previousSunday < 25;
 
      return false; // this line never gonna happend
  }
